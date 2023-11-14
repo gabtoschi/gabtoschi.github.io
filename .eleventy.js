9d0372deac2256e2fb67c4d1dc7f7336057cdd21
Gabriel Toschi <gabriel.toschi@hotmail.com>
2023-11-14 19:26:08 -0300

    home header

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
-const { EleventyI18nPlugin } = require("@11ty/eleventy");
+const { EleventyI18nPlugin } = require('@11ty/eleventy');
 
-const sass = require("sass");
-const path = require("path");
+const sass = require('sass');
+const path = require('path');
+const fs = require('fs');
 
 const DEFAULT_LANG = 'en-US'
 
@@ -10,20 +11,22 @@ module.exports = function(eleventyConfig) {
     defaultLanguage: DEFAULT_LANG
   });
 
-  eleventyConfig.addGlobalData("lang", DEFAULT_LANG);
+  eleventyConfig.addGlobalData('lang', DEFAULT_LANG);
 
-  eleventyConfig.addTemplateFormats("scss");
-  eleventyConfig.addExtension("scss", {
-    outputFileExtension: "css",
+  eleventyConfig.addPassthroughCopy('content/img');
+
+  eleventyConfig.addTemplateFormats('scss');
+  eleventyConfig.addExtension('scss', {
+    outputFileExtension: 'css',
 
     compile: async function (inputContent, inputPath) {
       let parsed = path.parse(inputPath);
-      if (parsed.name.startsWith("_")) {
+      if (parsed.name.startsWith('_')) {
         return;
       }
 
       let result = sass.compileString(inputContent, {
-        loadPaths: [parsed.dir || "."],
+        loadPaths: [parsed.dir || '.'],
         sourceMap: false,
       });
 
@@ -35,12 +38,22 @@ module.exports = function(eleventyConfig) {
     },
   });
 
+  eleventyConfig.addShortcode('svg', function (file) {
+    const relativeFilePath = `./content/img/${file}.svg`;
+    const data = fs.readFileSync(relativeFilePath, (err, contents) => {
+      if (err) return err;
+      return contents;
+    });
+
+    return data.toString('utf8');
+  });
+
   return {
     dir: {
-			input: "content",          // default: "."
-			includes: "../_includes",  // default: "_includes"
-			data: "../_data",          // default: "_data"
-			output: "_site"
+			input: 'content',          // default: '.'
+			includes: '../_includes',  // default: '_includes'
+			data: '../_data',          // default: '_data'
+			output: '_site'
 		},
   }
 };
